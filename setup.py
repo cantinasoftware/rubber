0dbb6021c3d08676a598e2b8c061d2970522a28d
Stéphane JAIS <github@stephanejais.com>
2012-08-03 18:48:41 +0200

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rubber',
-    version='0.1.4',
+    version='0.1.5',
     description='Elasticsearch client with Django support.',
     author='Stéphane JAIS',
     author_email='stephane@cantinasoftware.com',
